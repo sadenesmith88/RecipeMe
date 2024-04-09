b34f04a39a900054a1d9bcaefd05b7c9ef2277b3
Sade Nesmith <sadework21@gmail.com>
2024-04-08 21:38:56 -0400

    Initial Commit

---
+++ RecipeMe/RecipeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeMeApp.swift
+//  RecipeMe
+//
+//  Created by sade on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
