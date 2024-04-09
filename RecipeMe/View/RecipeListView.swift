b467f8fbdcfc9bb0f0ad0aa59fddafc164ddeb64
Sade Nesmith <sadework21@gmail.com>
2024-04-09 14:42:00 -0400

    created recipe model

---
+++ RecipeMe/View/RecipeListView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeListView.swift
+//  RecipeMe
+//
+//  Created by sade on 4/9/24.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeListView()
+    }
+}
